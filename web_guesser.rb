66a9dfaf1f7a53338a1cb467f75e0ac3c3b6d492
Andrew Watkins <andrew.watkinz@gmail.com>
2014-07-21 14:57:03 -0600

    my guessing game works

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,8 +1,22 @@
 require 'sinatra'
 require 'sinatra/reloader'
+require 'pry'
 
-number = rand(101)
+SECRET_NUMBER = rand(101)
 
 get  '/' do
-  erb :index, :locals => { :number => number }
+  guess   = params['guess'].to_i
+  message = check_guess(guess)
+  erb :index, :locals => { :message => message }
+end
+
+
+def check_guess(guess)
+  'please guess'
+  case
+  when guess == 0 then "please guess"
+  when guess > SECRET_NUMBER then 'too high'
+  when guess < SECRET_NUMBER then 'too low'
+  when guess == SECRET_NUMBER then 'you guessed correctly'
+  end
 end
